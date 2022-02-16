c70de3c56023516df6020f0736f48839bc754a07
John Little <john.little@duke.edu>
2022-02-16 16:37:19 -0500

    relative

--- functions_example.R
+++ functions_example.R
@@ -1,4 +1,4 @@
-source("functions_to_use_other.R")  # if you don't use RStudio projects, you may have to setwd() or use a full file path
+source("functions_to_use_other.R")  # if you don't use RStudio projects, you may have to use a full file path, or relative file path -- relative to the main script
 
 answer <-  5 * 5
 
