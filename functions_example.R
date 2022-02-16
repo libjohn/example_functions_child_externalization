0bac4647c66d7444466704cb9657dd184f650929
John Little <john.little@duke.edu>
2022-02-16 16:35:26 -0500

    source

--- functions_example.R
+++ functions_example.R
@@ -1,4 +1,4 @@
-source("functions_to_use_other.R")  # if you don't use RStudio projects, you may have to setwd() or put a full path
+source("functions_to_use_other.R")  # if you don't use RStudio projects, you may have to setwd() or use a full file path
 
 answer <-  5 * 5
 
---
+++ functions_to_use_other.R
@@ -0,0 +1,11 @@
+plotcars <- function(df) {
+  plot(df)
+}
+
+## ----  multiply_by_two
+
+multiplybytwo <- function(n) {
+    return(2 * n)
+}
+
+
